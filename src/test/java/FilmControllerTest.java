766f10173f4fdac4eefcafdd8fd1b06d94d06346
Evstafev <tehnol25@yandex.ru>
2025-04-07 11:17:35 +0700

    ТЗ 10

---
+++ src/test/java/FilmControllerTest.java
@@ -0,0 +1,100 @@
+import org.apache.logging.log4j.util.Strings;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.controller.ValidateController;
+import ru.yandex.practicum.exception.ValidationException;
+import ru.yandex.practicum.exception.ValidationNullException;
+import ru.yandex.practicum.model.Filme;
+
+import java.time.LocalDate;
+
+public class FilmControllerTest {
+
+    Exception exception;
+    ValidateController validate = new ValidateController();
+    Filme film;
+
+    @BeforeEach
+    void setUp() {
+        film = new Filme(1L, "name", "описание ", LocalDate.now().minusYears(3), 20);
+    }
+
+    @Test
+    public void FilmNullName() {
+        film.setName(null);
+        exception = Assertions.assertThrows(ValidationNullException.class, () -> validate.ValidateFilm(film));
+        Assertions.assertEquals("название пустое", exception.getMessage());
+    }
+
+    @Test
+    public void FilmNullDescription() {
+        film.setDescription(null);
+        exception = Assertions.assertThrows(ValidationNullException.class, () -> validate.ValidateFilm(film));
+        Assertions.assertEquals("описание пустое", exception.getMessage());
+    }
+
+    @Test
+    public void FilmNullReleaseDate() {
+        film.setReleaseDate(null);
+        exception = Assertions.assertThrows(ValidationNullException.class, () -> validate.ValidateFilm(film));
+        Assertions.assertEquals("дата выпуска не заполнена", exception.getMessage());
+    }
+
+    @Test
+    public void FilmEmptyName() {
+        film.setName("");
+        exception = Assertions.assertThrows(ValidationException.class, () -> validate.ValidateFilm(film));
+        Assertions.assertEquals("название пустое", exception.getMessage());
+    }
+
+    @Test
+    public void FilmEmptyDescription() {
+        film.setDescription("");
+        exception = Assertions.assertThrows(ValidationException.class, () -> validate.ValidateFilm(film));
+        Assertions.assertEquals("описание пустое", exception.getMessage());
+    }
+
+    @Test
+    public void FilmMax200Description() {
+        film.setDescription(Strings.repeat("*", 220));
+        exception = Assertions.assertThrows(ValidationException.class, () -> validate.ValidateFilm(film));
+        Assertions.assertEquals("превышает количество символов", exception.getMessage());
+    }
+
+    @Test
+    public void FilmMax201Description() {
+        film.setDescription(Strings.repeat("*", 201));
+        exception = Assertions.assertThrows(ValidationException.class, () -> validate.ValidateFilm(film));
+        Assertions.assertEquals("превышает количество символов", exception.getMessage());
+    }
+
+    @Test
+    public void FilmMax199Description() {
+        film.setDescription(Strings.repeat("*", 199));
+        Assertions.assertDoesNotThrow(() -> validate.ValidateFilm(film));
+    }
+
+    @Test
+    public void FilmNotReleaseDate() {
+        film.setReleaseDate(LocalDate.MIN);
+        exception = Assertions.assertThrows(ValidationException.class, () -> validate.ValidateFilm(film));
+        Assertions.assertEquals("дата выпуска раньше 28 декабря 1895", exception.getMessage());
+    }
+
+    @Test
+    public void FilmPositiveDuration() {
+        film.setDuration(-4);
+        exception = Assertions.assertThrows(ValidationException.class, () -> validate.ValidateFilm(film));
+        Assertions.assertEquals("продолжительность <= 0", exception.getMessage());
+    }
+
+    @Test
+    public void FilmIs0Duration() {
+        film.setDuration(0);
+        exception = Assertions.assertThrows(ValidationException.class, () -> validate.ValidateFilm(film));
+        Assertions.assertEquals("продолжительность <= 0", exception.getMessage());
+    }
+
+
+}
---
+++ src/test/java/ValidateTest.java
@@ -0,0 +1,82 @@
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.apache.logging.log4j.util.Strings;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.model.Filme;
+import ru.yandex.practicum.model.User;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class ValidateTest {
+    private Validator validator;
+
+    @BeforeEach
+    void setUp() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    @Test
+    void ValidatorUserOk() {
+        User user = new User(1L, "name@yabex.ru", "log", "name", LocalDate.now().minusYears(3));
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    void ValidatorUserTest() {
+        User user = new User(1L, "nameyabex.ru", "log", "name", LocalDate.now().plusYears(3));
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertFalse(violations.isEmpty());
+        assertThat(violations).hasSize(2);
+        assertThat(violations).extracting(ConstraintViolation::getMessage).containsExactlyInAnyOrder("email не соотвествует формату",
+                "дата из будущего");
+    }
+
+    @Test
+    void ValidatorUserNotNull() {
+        User user = new User(1L, null, null, "name", null);
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertFalse(violations.isEmpty());
+        assertThat(violations).hasSize(3);
+        assertThat(violations).extracting(ConstraintViolation::getMessage).containsExactlyInAnyOrder("email не заполнен",
+                "логин не заполнен", "дата не заполнена");
+    }
+
+    @Test
+    void ValidatorFilmOk() {
+        Filme film = new Filme(1L, "kino", "kino o kine", LocalDate.of(1990, 02, 22), 25);
+        Set<ConstraintViolation<Filme>> violations = validator.validate(film);
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    void ValidatorFilmTest() {
+        Filme film = new Filme(1L, "kino", Strings.repeat("*", 220),
+                LocalDate.now().plusYears(3), -12);
+        Set<ConstraintViolation<Filme>> violations = validator.validate(film);
+        assertFalse(violations.isEmpty());
+        assertThat(violations).hasSize(4);
+        assertThat(violations).extracting(ConstraintViolation::getMessage).containsExactlyInAnyOrder("не правильное число символов",
+                "дата из будущего", "неверное значение", "должно быть не меньше 1");
+    }
+
+    @Test
+    void ValidatorFilmNotNull() {
+        Filme film = new Filme(1L, null, null,
+                null, 0);
+        Set<ConstraintViolation<Filme>> violations = validator.validate(film);
+        assertFalse(violations.isEmpty());
+        assertThat(violations).hasSize(5);
+        assertThat(violations).extracting(ConstraintViolation::getMessage).containsExactlyInAnyOrder("название не заполнено",
+                "должно быть не меньше 1", "дата пустая", "описание пустое", "неверное значение");
+    }
+
+}
